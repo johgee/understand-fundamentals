45b5115ec0bc5df12293893c96972688b8a79e83
Grace Joh <graceeunaejoh@gmail.com>
2022-06-04 01:32:17 -0400

    exercise 1 2 3

---
+++ exercise2.js
@@ -0,0 +1,10 @@
+var number = 5;
+
+if (number === 5) {
+  console.log("number is 5");
+} else {
+  console.log("number is not 5");
+}
+
+// read the error message to find out where the problem is
+// unexpected token else : ; it's done in javascript
